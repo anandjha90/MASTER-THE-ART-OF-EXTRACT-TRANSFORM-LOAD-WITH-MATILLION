04b085b6d6fa90cde6dbc7c78feb94a75dd95d26
Anand Jha <anandjha_1990@yahoo.com>
2025-01-25 04:07:20 +0530

    Update Load_From_API.sql

--- Matillion+MasterClass/Load_From_API.sql
+++ Matillion+MasterClass/Load_From_API.sql
@@ -4,13 +4,11 @@
 -- Paramter : Other
 -- Value : generateschemaoptions=XPathOutputPrefix;ignorecontenttype=True;key=e0701317f86944a49a6215639252401;q=${v_City};aqi=no;
 
-select * from replication_db."WEATHER_api_extract";
-
-select * from replication_db."WeatherAPI"
+select * from PC_MATILLION_DEV_DB.SRC.WEATHER_API_EXTRACT_TABLE;
 
 SELECT distinct 
 "Data Value":"location":name::VARCHAR AS Location,
 "Data Value":"current":temp_c::VARCHAR AS Temperature,
 "Data Value":"location":localtime::VARCHAR AS Record_Date
-FROM replication_db."WEATHER_api_extract" as s 
+FROM PC_MATILLION_DEV_DB.SRC.WEATHER_API_EXTRACT_TABLE as s 
 ,LATERAL FLATTEN(input => "Data Value");
